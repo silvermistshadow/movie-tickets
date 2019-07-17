e09a32acd144efba7a0b5f453eeffc33e7b3a704
silvermistshadow <bmead72@gmail.com>
2019-07-17 10:40:54 -0700

    start working on javascript

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+//Back end starts here
+function Ticket(MovieName, timeOfDay, age) {
+  this.MovieName = MovieName;
+  this.timeOfDay = timeOfDay;
+  this.age = age;
+}
+
+Ticket.prototype.getPrice = function() {
+  
+}
+
+function MovieName(name, isFirstRelease) {
+  this.name = name;
+  this.isFirstRelease = isFirstRelease;
+}
+
+
+
+
+
+
+
+
+//User interface past this point
